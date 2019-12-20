23a14769a794c74f3ebed3e4d95c7361384f2974
Horea Porutiu <horeaporutiu@gmail.com>
2019-12-20 11:03:31 -0800

    update script for query

--- first-network/scripts/script.sh
+++ first-network/scripts/script.sh
@@ -98,19 +98,7 @@ if [ "${NO_CHAINCODE}" != "true" ]; then
 
 	# Query chaincode on peer0.org1
 	echo "Querying chaincode on peer0.org1..."
-	chaincodeQuery 0 1 100
-
-	# Invoke chaincode on peer0.org1 and peer0.org2
-	echo "Sending invoke transaction on peer0.org1 peer0.org2..."
-	chaincodeInvoke 0 1 0 2
-	
-	## Install chaincode on peer1.org2
-	echo "Installing chaincode on peer1.org2..."
-	installChaincode 1 2
-
-	# Query on chaincode on peer1.org2, check if the result is 90
-	echo "Querying chaincode on peer1.org2..."
-	chaincodeQuery 1 2 90
+	chaincodeQuery 0 1
 	
 fi
 
