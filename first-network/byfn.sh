ebab3b9dafc762d896a65e1cae47f596b4f13508
Aditya Prakash Joshi <46881822+adityainterwork@users.noreply.github.com>
2020-01-21 10:21:08 +0530

    fixed file name

--- first-network/byfn.sh
+++ first-network/byfn.sh
@@ -322,7 +322,7 @@ function replacePrivateKey() {
   sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
   # If MacOSX, remove the temporary backup of the docker-compose file
   if [ "$ARCH" == "Darwin" ]; then
-    rm docker-compose-e2e.yamlt
+    rm docker-compose-e2e.yaml
   fi
 }
 
