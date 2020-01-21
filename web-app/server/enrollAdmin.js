7d4d8a4590f9e11f95d738a24c6646b7040a247d
Horea Porutiu <horeaporutiu@gmail.com>
2020-01-21 12:48:04 -0800

    typo in crypto materials for org2, enrollAdmin should import username

--- web-app/server/enrollAdmin.js
+++ web-app/server/enrollAdmin.js
@@ -17,6 +17,7 @@ const config = JSON.parse(configJSON);
 var connection_file = config.connection_file;
 var appAdmin = config.appAdmin;
 var appAdminSecret = config.appAdminSecret;
+var userName = config.userName;
 var orgMSPID = config.orgMSPID;
 var caName = config.caName;
 
@@ -34,7 +35,7 @@ async function main() {
         const wallet = new FileSystemWallet(walletPath);
 
         // Check to see if we've already enrolled the admin user.
-        const adminExists = await wallet.exists(appAdmin);
+        const adminExists = await wallet.exists(userName);
         if (adminExists) {
             console.log('An identity for the admin user "admin" already exists in the wallet');
             return;
@@ -44,11 +45,11 @@ async function main() {
 
         const enrollment = await ca.enroll({ enrollmentID: appAdmin, enrollmentSecret: appAdminSecret });
         const identity = X509WalletMixin.createIdentity(orgMSPID, enrollment.certificate, enrollment.key.toBytes());
-        wallet.import(appAdmin, identity);
-        console.log('msg: Successfully enrolled admin user ' + appAdmin + ' and imported it into the wallet');
+        wallet.import(userName, identity);
+        console.log('msg: Successfully enrolled admin user ' + userName + ' and imported it into the wallet');
 
     } catch (error) {
-        console.error(`Failed to enroll admin user: ${appAdmin} ${error}`);
+        console.error(`Failed to enroll admin user: ${userName} ${error}`);
         process.exit(1);
     }
 }
